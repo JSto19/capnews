24a9dfd08cd6d78081d11d78d7a1fa975db2de70
JSto19 <55250301+JSto19@users.noreply.github.com>
2021-04-22 21:18:50 -0500

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['stoney19llc@outlook.com']
+    ADMINS = ['youremail@mail.com']
     POSTS_PER_PAGE = 25
